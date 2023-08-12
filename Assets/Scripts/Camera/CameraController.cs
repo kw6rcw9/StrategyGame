be1582a1a61c7a29857a25f55e00d75d5a4ac054
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-08-13 00:39:54 +0300

    added camera controller

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,32 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private float _rotateSpeed = 10.0f, _speed = 10.0f, _zoomSpeed = 10.0f;
+        private float _mult = 1f;
+        private void Update()
+        {
+            float hor = Input.GetAxis("Horizontal");
+            float ver = Input.GetAxis("Vertical");
+            float _rotate = 0f;
+            if (Input.GetKey(KeyCode.Q))
+            {
+                _rotate = -1f;
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                _rotate = 1f;
+            }
+
+            _mult = Input.GetKey(KeyCode.LeftShift) ? 2f : 1f;
+            transform.Rotate(Vector3.up * _rotateSpeed * Time.deltaTime * _rotate * _mult, Space.World);
+            transform.Translate(new Vector3(hor,0, ver) * Time.deltaTime * _mult * _speed, Space.Self);
+
+            transform.position += transform.up * _zoomSpeed * Time.deltaTime * Input.GetAxis("Mouse ScrollWheel");
+            transform.position = new Vector3(transform.position.x,Mathf.Clamp(transform.position.y, -25f, 25f), transform.position.z);
+        }
+    }
+}
