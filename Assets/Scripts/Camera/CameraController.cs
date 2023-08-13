8ea2cc68fbbca76bc7930327d30689ca8d138f73
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-08-14 00:07:40 +0300

    updated camera movement

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -9,8 +9,7 @@ namespace Camera
         private float _mult = 1f;
         private void Update()
         {
-            float hor = Input.GetAxis("Horizontal");
-            float ver = Input.GetAxis("Vertical");
+            
             float _rotate = 0f;
             if (Input.GetKey(KeyCode.Q))
             {
@@ -23,10 +22,22 @@ namespace Camera
 
             _mult = Input.GetKey(KeyCode.LeftShift) ? 2f : 1f;
             transform.Rotate(Vector3.up * _rotateSpeed * Time.deltaTime * _rotate * _mult, Space.World);
-            transform.Translate(new Vector3(hor,0, ver) * Time.deltaTime * _mult * _speed, Space.Self);
-
             transform.position += transform.up * _zoomSpeed * Time.deltaTime * Input.GetAxis("Mouse ScrollWheel");
             transform.position = new Vector3(transform.position.x,Mathf.Clamp(transform.position.y, -25f, 25f), transform.position.z);
+           
+            float hor = 0f, ver = 0f;
+            
+            if (Input.mousePosition.x < 5f)
+                hor = -1f;
+            if (Input.mousePosition.y < 5f)
+                ver = -1f;
+            
+            if (Input.mousePosition.x > Screen.width - 5f) 
+                hor = 1f;
+            if (Input.mousePosition.y > Screen.height - 5f)
+                ver = 1f;
+ 
+            transform.Translate(new Vector3(hor, 0, ver) * Time.deltaTime * _mult * _speed, Space.Self);
         }
     }
 }
